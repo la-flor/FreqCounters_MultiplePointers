077162c905ff3243bfdde2b61788cfb87e01cbb3
lafBot <64456835+lafBot@users.noreply.github.com>
2021-03-18 09:46:35 -0600

    Completed twoArrayObject()

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,10 @@
-// add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keys, values) {
+
+    return keys.reduce((obj, cur, idx) => {
+      obj[cur] = idx < values.length ? values[idx] : null;
+      return obj;
+    }, {});
+    
 }
+
+module.exports = { twoArrayObject }
