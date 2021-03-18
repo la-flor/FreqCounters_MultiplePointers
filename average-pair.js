20106bd16f9d3105710382dac73c4daaa82daf6e
lafBot <64456835+lafBot@users.noreply.github.com>
2021-03-17 22:42:32 -0600

    Completed averagePair() with testing

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,22 @@
-// add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(arr, avg) {
+    // first return false if the length of the array does not allow for a pair
+    if (arr.length < 2) return false;
+    /* since the average of a pair is the sum divided by 2
+        we will calculate the sum, and look for that to find the pair that matches */
+    const total = avg * 2
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+        if (sum === total) return true;
+        if (sum < total) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return false;
 }
+
+module.exports = { averagePair }
