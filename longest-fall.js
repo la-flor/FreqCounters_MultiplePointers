9bd4f696df4d25bb9e60a9ffe0d15c92c37f17d1
lafBot <64456835+lafBot@users.noreply.github.com>
2021-03-18 09:45:20 -0600

    Completed longestFall()

--- longest-fall.js
+++ longest-fall.js
@@ -1,3 +1,23 @@
-// add whatever parameters you deem necessary
-function longestFall() {
+function longestFall(nums) {
+    let counter = 1;
+    let maxCounter = 0;
+  
+    if (nums.length === 0) {
+      return 0;
+    }
+  
+    for (let i = 1; i < nums.length; i++) {
+      // if the current number is < previous number
+      if (nums[i] < nums[i - 1]) {
+        counter++;
+        maxCounter = Math.max(counter, maxCounter);
+      } else {
+        counter = 1;
+      }
+    }
+  
+    // 1 is the default val
+    return maxCounter || 1;
 }
+
+module.exports = { longestFall }
