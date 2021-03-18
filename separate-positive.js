98efc3c67c886bdd88d261b931b79a5d471554c0
lafBot <64456835+lafBot@users.noreply.github.com>
2021-03-18 09:46:07 -0600

    Completed separatePositive()

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,22 @@
-// add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+  
+    while (left < right) {
+      if (nums[left] < 0 && nums[right] > 0) {
+  
+        var temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+  
+        left += 1;
+        right -= 1;
+      } else {
+        if (nums[left] > 0) left += 1;
+        else right -= 1;
+      }
+    }
+    return nums;
 }
+
+module.exports = { separatePositive }
