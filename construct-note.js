0391b6f9e12daaa34f753318f160117e94148c9d
lafBot <64456835+lafBot@users.noreply.github.com>
2021-03-18 08:56:00 -0600

    Completed constructionNote()

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,21 @@
-// add whatever parameters you deem necessary
-function constructNote() {
+function freqCounter(arr) {
+    const freqObj = {};
+    for (let ltr of arr) {
+        freqObj[ltr] = freqObj[ltr] + 1 || 1;
+    }
+    return freqObj;
 }
+
+function constructNote(msg, letters) {
+    if (letters.length === 0) return false;
+    const msgObj = freqCounter(msg.split(""));
+    const ltrObj = freqCounter(letters.split(""));
+    for (let ltr in msgObj) {
+      if (msgObj[ltr] > ltrObj[ltr]) {
+        return false;
+      }
+    }
+    return true;
+}
+
+module.exports = { constructNote }
